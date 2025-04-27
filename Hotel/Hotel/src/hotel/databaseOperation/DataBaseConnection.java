73ce39409606c60d35f0195e542b9fab1968995c
EnigmaticCipher <aimoh25ai@gmail.com>
2025-04-27 20:20:39 +0100

    initial commit: Hotel project files sorry for the late

---
+++ Hotel/Hotel/src/hotel/databaseOperation/DataBaseConnection.java
@@ -0,0 +1,30 @@
+package hotel.databaseOperation;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+/**
+ *
+ * @author Faysal Ahmed
+ */
+
+public class DataBaseConnection {
+
+    static String url = "jdbc:mysql://localhost:3306/hotel?useUnicode=true" +
+            "&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&" +
+            "serverTimezone=UTC";
+
+    public static Connection connectTODB() {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            return DriverManager.getConnection(url, "root", "root"); // * Hardcoded
+        } catch (Exception e) {
+
+            System.err.println("Connection error");
+            e.printStackTrace();
+            return null;
+        }
+
+    }
+
+}
